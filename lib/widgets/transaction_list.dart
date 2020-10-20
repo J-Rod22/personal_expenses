0e356c7dc8dd41a4a2285b69b8356a03f9f1bc4b
Jerry Rodriguez <jrvj01@hotmail.com>
2020-10-20 09:47:31 -0400

    89. working with listviews

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,9 +12,8 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((tx) {
+        child: ListView.builder(
+          itemBuilder: (ctx, index) {
             return Card(
               child: Row(
                 children: <Widget>[
@@ -33,7 +32,7 @@ class TransactionList extends StatelessWidget {
                       10,
                     ),
                     child: Text(
-                      '\$${tx.amount}',
+                      '\$${transactions[index].amount}',
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 20,
@@ -45,14 +44,14 @@ class TransactionList extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
                       Text(
-                        tx.title,
+                        transactions[index].title,
                         style: TextStyle(
                           fontSize: 16,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
                       Text(
-                        DateFormat.yMMMd().format(tx.date),
+                        DateFormat.yMMMd().format(transactions[index].date),
                         style: TextStyle(
                           color: Colors.grey,
                         ),
@@ -62,9 +61,9 @@ class TransactionList extends StatelessWidget {
                 ],
               ),
             );
-          }).toList(),
+          },
+          itemCount: transactions.length,
         ),
-      ),
     );
   }
 }
